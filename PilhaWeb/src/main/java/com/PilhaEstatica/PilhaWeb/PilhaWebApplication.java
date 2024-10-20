869d94df641f61599023a4a0dc01c50e7e314f95
luccasilvx <lucafreitasilva@gmail.com>
2024-10-20 12:33:03 -0300

    subindo projeto

---
+++ PilhaWeb/src/main/java/com/PilhaEstatica/PilhaWeb/PilhaWebApplication.java
@@ -0,0 +1,13 @@
+package com.PilhaEstatica.PilhaWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilhaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilhaWebApplication.class, args);
+	}
+
+}
---
+++ PilhaWeb/src/test/java/com/PilhaEstatica/PilhaWeb/PilhaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PilhaEstatica.PilhaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilhaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
